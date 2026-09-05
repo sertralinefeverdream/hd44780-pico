001363a3173a7dddecd06eda8e2ebacbb4af1f17
Adrian Graham <84514425+sertralinefeverdream@users.noreply.github.com>
2026-09-05 01:04:36 +0100

    Settled on interface. pulse_enable() and set_buffer_raw() impl.

--- include/hd44780_lib/lcd.h
+++ include/hd44780_lib/lcd.h
@@ -4,10 +4,6 @@
 #include <cstdint>
 #include <string_view>
 
-#include "pico/stdlib.h"
-#include "hardware/gpio.h"
-
-
 namespace hd44780pico {
     enum class OperationMode {
         FOUR_BIT,
@@ -29,55 +25,54 @@ namespace hd44780pico {
         SHIFT_CURSOR,
         SHIFT_DISPLAY,
     };
-    
-    struct ParallelPinMap { 
-        std::uint8_t rs{};
-        std::uint8_t rw{};
-        std::uint8_t e{};
-        std::uint8_t backlight{};
-        std::uint8_t d7{};
-        std::uint8_t d6{};
-        std::uint8_t d5{};
-        std::uint8_t d4{};
-        std::uint8_t d3{};
-        std::uint8_t d2{};
-        std::uint8_t d1{};
-    }; 
-    
+   
     class LcdDisplay {
     public:
         explicit LcdDisplay(OperationMode op_mode=OperationMode::FOUR_BIT, LineMode line_mode=LineMode::TWO_LINES_5_8);
+
         void clear_display();
         void return_home();
-        void putc(char c);
-        void puts(std::string_view s);
+        void putc(const char c);
+        void puts(const std::string_view s);
         void set_display_enabled(bool enabled);
         void set_backlight_enabled(bool enabled);
-        void set_cursor_blink(bool enabled);
+        void set_blink_enabled(bool enabled);
+        void shift_cursor(int n);
+        void shift_display(int n);
         void cursor_goto(const std::uint8_t col, const std::uint8_t row);
         
     protected:
+        virtual const std::uint16_t buffer() const;
+        virtual void send_buffer() const = 0;
+        virtual void init_io() const = 0;
+
+    private:
+        enum Instruction : std::uint16_t { // RS, RW, DB7, DB6, DB5, DB4, DB3, DB2, DB1, DB0
+            CLEAR_DISPLAY = 0b00'0000'0001,
+            RETURN_HOME = 0b00'0000'0010,
+            ENTRY_MODE_SET = 0b00'0000'0100,
+            DISPLAY_CONTROL = 0b00'0000'1000,
+            CURSOR_DISPLAY_SHIFT = 0b00'0001'0000,
+            FUNCTION_SET = 0b00'0010'0000,
+            SET_CGRAM = 0b00'0100'0000,
+            SET_DDRAM = 0b00'1000'0000,
+        };
+        
+        std::uint16_t buffer_{}; // 12 bits: BACKLIGHT, E, RS, RW, D0, D1, D2, D3, D4, D5, D6, D7
         bool backlight_enabled_{true};
         bool display_enabled_{true};
-        bool cursor_blink_enabled_{true};
-        bool shift_display_on_write_{false};
+        bool blink_enabled_{true};
+        bool shift_on_write_{false};
         ShiftType shift_type_{ShiftType::SHIFT_CURSOR};
         Direction cursor_dir_{Direction::RIGHT};
         OperationMode operation_mode_;
-        LineMode line_mode_{LineMode::TWO_LINES_5_8};
+        LineMode line_mode_;
 
-        virtual void write_byte(const std::uint8_t byte, const bool pulse_enable=true) const = 0;
-        virtual void write_nibble(const std::uint8_t nibble, const bool pulse_enable=true) const = 0;
-        virtual void pulse_enable() const = 0;
-        virtual void init_io() const = 0;
-        virtual void init_sequence() const = 0;
-    };
-    
-    class LcdDisplayParallel : public LcdDisplay {
-    public:
-        explicit LcdDisplayParallel(ParallelPinMap pin_map, OperationMode op_mode=OperationMode::FOUR_BIT, LineMode line_mode=LineMode::TWO_LINES_5_8);
-    private:
-        ParallelPinMap pin_map_;
+        void execute_instruction(std::uint16_t instr);
+        void set_buffer_raw(uint16_t buf);
+        void pulse_enable();
+        void init_sequence();
+   
     };
 }
 
--- src/lcd.cpp
+++ src/lcd.cpp
@@ -1,8 +1,28 @@
 #include "../include/hd44780_lib/lcd.h"
 
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
 using namespace hd44780pico;
 
+namespace {
+    constexpr int bm_enable{0b01'0000'0000};    
+    constexpr int delay_ms_pulse_enable{2};
+}
+// Base Class
 LcdDisplay::LcdDisplay(OperationMode op_mode, LineMode line_mode)
     : operation_mode_{op_mode}
     , line_mode_{line_mode} {} 
 
+// private impls
+void LcdDisplay::set_buffer_raw(uint16_t buf) { 
+    buffer_ = buf;
+}
+
+void LcdDisplay::pulse_enable() {
+    buffer_ |= bm_enable; 
+    send_buffer();
+    sleep_us(delay_ms_pulse_enable);
+    buffer_ &= ~(bm_enable);
+    send_buffer();
+}
