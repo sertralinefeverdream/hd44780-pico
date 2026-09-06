a6211c0a7751885538fe7be99090f8d2a84a0729
Adrian Graham <84514425+sertralinefeverdream@users.noreply.github.com>
2026-09-06 09:24:34 +0100

    Tidying up before preparing merge to main.

--- src/lcd.cpp
+++ src/lcd.cpp
@@ -1,4 +1,4 @@
-#include "../include/hd44780_lib/lcd.h"
+#include "hd44780_pico/lcd.h"
 
 #include <string_view>
 #include <algorithm>
@@ -10,42 +10,42 @@ using namespace hd44780pico;
 
 namespace {
     // Bitmasks
-    constexpr uint16_t bm_clear_buffer_data{0b1100'0000'0000};
-    constexpr uint16_t bm_upper_nibble{0x0F0};
-    constexpr uint16_t bm_lower_nibble{0x00F};
-    constexpr uint16_t bm_rs_rw{0x300};
+    constexpr std::uint16_t bm_clear_buffer_data{0b1100'0000'0000};
+    constexpr std::uint16_t bm_upper_nibble{0x0F0};
+    constexpr std::uint16_t bm_lower_nibble{0x00F};
+    constexpr std::uint16_t bm_rs_rw{0x300};
     
-    constexpr uint16_t bm_clear_display{0b1};
-    constexpr uint16_t bm_return_home{0b10};
+    constexpr std::uint16_t bm_clear_display{0b1};
+    constexpr std::uint16_t bm_return_home{0b10};
 
-    constexpr uint16_t bm_ems{0b00'0000'0100};
-    constexpr uint16_t bm_ems_increment{0b10};
-    constexpr uint16_t bm_ems_shift_display{0b1};
+    constexpr std::uint16_t bm_ems{0b00'0000'0100};
+    constexpr std::uint16_t bm_ems_increment{0b10};
+    constexpr std::uint16_t bm_ems_shift_display{0b1};
     
-    constexpr uint16_t bm_dc{0b00'0000'1000};
-    constexpr uint16_t bm_dc_display_on{0b100};
-    constexpr uint16_t bm_dc_cursor_on{0b10};
-    constexpr uint16_t bm_dc_blinking_on{0b1};
+    constexpr std::uint16_t bm_dc{0b00'0000'1000};
+    constexpr std::uint16_t bm_dc_display_on{0b100};
+    constexpr std::uint16_t bm_dc_cursor_on{0b10};
+    constexpr std::uint16_t bm_dc_blinking_on{0b1};
     
-    constexpr uint16_t bm_cds{0b00'0001'0000};
-    constexpr uint16_t bm_cds_shift_screen{0b1000};
-    constexpr uint16_t bm_cds_dir_right{0b100};
+    constexpr std::uint16_t bm_cds{0b00'0001'0000};
+    constexpr std::uint16_t bm_cds_shift_screen{0b1000};
+    constexpr std::uint16_t bm_cds_dir_right{0b100};
     
-    constexpr uint16_t bm_fs{0b00'0010'0000};
-    constexpr uint16_t bm_fs_8_bit_mode{0b10000};
-    constexpr uint16_t bm_fs_two_lines{0b1000};
-    constexpr uint16_t bm_fs_5_by_10_font{0b100};
+    constexpr std::uint16_t bm_fs{0b00'0010'0000};
+    constexpr std::uint16_t bm_fs_8_bit_mode{0b10000};
+    constexpr std::uint16_t bm_fs_two_lines{0b1000};
+    constexpr std::uint16_t bm_fs_5_by_10_font{0b100};
     
-    constexpr uint16_t bm_write_ddram_data{0b10'0000'0000};
-    constexpr uint16_t bm_set_ddram_addr{0b00'1000'0000};
-    constexpr uint16_t bm_ddram_addr{0b00'0111'1111};
-    constexpr uint16_t bm_ddram_data{0b00'1111'1111};
+    constexpr std::uint16_t bm_write_ddram_data{0b10'0000'0000};
+    constexpr std::uint16_t bm_set_ddram_addr{0b00'1000'0000};
+    constexpr std::uint16_t bm_ddram_addr{0b00'0111'1111};
+    constexpr std::uint16_t bm_ddram_data{0b00'1111'1111};
     
-    constexpr uint16_t bm_backlight{0b1000'0000'0000};
-    constexpr uint16_t bm_enable_e{0b0100'0000'0000};
+    constexpr std::uint16_t bm_backlight{0b1000'0000'0000};
+    constexpr std::uint16_t bm_enable_e{0b0100'0000'0000};
     
-    constexpr uint16_t bm_init{0b00'0011'0000};
-    constexpr uint16_t bm_blank{0};
+    constexpr std::uint16_t bm_init{0b00'0011'0000};
+    constexpr std::uint16_t bm_blank{0};
 
     // Delays
     constexpr int delay_us_clear_display{2};
